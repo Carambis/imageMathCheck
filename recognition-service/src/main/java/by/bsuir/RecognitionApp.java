0fe75ab301bbfc3e39a076f3bc4517cf71f1024b
Carambis <lepeshko1997@mail.ru>
2019-05-08 22:18:17 +0300

    Initial commit

---
+++ recognition-service/src/main/java/by/bsuir/RecognitionApp.java
@@ -0,0 +1,13 @@
+package by.bsuir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecognitionApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecognitionApp.class, args);
+    }
+
+}
